ca5ea968d9ae8c5b8a42165d07aa303862a9705f
Donovan Preston <46048+fzzzy@users.noreply.github.com>
2025-08-24 09:20:39 -0400

    simplify

--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 
 import asyncio
 from rich.console import Console
-#from introspection import introspect_composio_client
 from hackernews_integration import get_hackernews_frontpage
-# from openai_integration import query_openai_for_city
 
 
 async def main():
